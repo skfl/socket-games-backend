cfc5cba60377593d04d0e21592dbda9180043b00
Edward Rogachkov <edik.rogachkov@gmail.com>
2023-03-12 11:02:40 +0400

    init commit

---
+++ src/main/java/ru/skfl/socketgames/SocketGamesApplication.java
@@ -0,0 +1,13 @@
+package ru.skfl.socketgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  SocketGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skfl/socketgames/SocketGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skfl.socketgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
